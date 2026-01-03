3dd6bfb04652c7f6a2a9be8fbde8d648a42103d8
TarekNexus <muhammadtarek5920@gmail.com>
2026-01-03 23:39:52 +0600

    Our Services done

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 import Blogs from "@/components/home/Blog";
 import Faq from "@/components/home/Faq";
 import Hero from "@/components/home/Hero";
+import Services from "@/components/home/Services";
 
 
 
 export default function Home() {
   return (
     <div className=" flex flex-col gap-4 ">
+      <Services></Services>
       <Faq></Faq>
    <Blogs></Blogs>
    <Hero></Hero>
